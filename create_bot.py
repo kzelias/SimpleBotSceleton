6a72a9e90489a0c094f891d4d3964f371a54493d
Elias <67437154+HiElias@users.noreply.github.com>
2023-03-08 17:46:00 +0300

    Update create_bot.py

--- create_bot.py
+++ create_bot.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 
 
-bot = Bot(token="5676704740:AAGd7Qg0gXqQli5WZo5RK_GJahSdcH2JK6o")
+bot = Bot(token="{YOUR_BOT_TOKEN}")
 bot.db_con = None
 bot.active_writer = None
 bot.previous_callback = None
